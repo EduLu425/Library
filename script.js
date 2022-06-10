cd0acec5305f57d20ed781858a26937667ac5447
Eduardo <ed.luna80@gmail.com>
2022-06-09 20:59:55 -0700

    Program addBookToLibrary function to manually add book cards in dom based on input

--- script.js
+++ script.js
@@ -12,14 +12,34 @@ function book(title, author, pages, read) {
 
 const contentDiv = document.getElementById('content');
 
+const bookCard = document.createElement('div');
+
+bookCard.style.color = 'black';
+
+bookCard.style.backgroundColor = 'white';
+
 
 function addBookToLibrary() {
+    while (contentDiv.firstChild) {
+        contentDiv.removeChild(contentDiv.lastChild)
+    }
     let newBook = new book();
     newBook.title = prompt('What is the title of this book?', '');
     newBook.author = prompt('Who wrote this book?', '');
     newBook.pages = prompt('How many pages is this book?', '');
     newBook.read = prompt('Have you read this book?', '');
     myLibrary.push(newBook);
+    for (let element of myLibrary) {
+        let content = `<h2>${element.title}</h2>
+        <p>by ${element.author}</p>
+         <p>${element.pages} pages</p>
+         <p>Read? ${element.read}</p>
+         `;
+        bookCard.innerHTML = content;
+        contentDiv.appendChild(bookCard.cloneNode(true));
+        
+
+    }
 }
 
 
