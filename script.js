601a52791b9f60fef5ab823f49b8d62906ac6ede
Eduardo <ed.luna80@gmail.com>
2022-06-13 16:39:10 -0700

    Library now generates book cards from html input
    Edit addBooktoLibrary function to generate a book info card once submit
    button is pressed. Put for of loop inside that function into its own
    generateLibrary helper function. Style dynamically generate book cards
    with javascript.

--- script.js
+++ script.js
@@ -14,38 +14,51 @@ const contentDiv = document.getElementById('content');
 
 const bookCard = document.createElement('div');
 
+let titleInput = document.getElementById('title');
+
+let authorInput = document.getElementById('author');
+
+let pagesInput = document.getElementById('pages');
+
+let readInput = document.getElementById('read');
+
 bookCard.style.color = 'black';
 
 bookCard.style.backgroundColor = 'white';
 
+bookCard.style.textAlign = 'center';
 
-function addBookToLibrary() {
-    while (contentDiv.firstChild) {
-        contentDiv.removeChild(contentDiv.lastChild)
-    }
-    let newBook = new book();
-    newBook.title = prompt('What is the title of this book?', '');
-    newBook.author = prompt('Who wrote this book?', '');
-    newBook.pages = prompt('How many pages is this book?', '');
-    newBook.read = prompt('Have you read this book?', '');
-    myLibrary.push(newBook);
+bookCard.style.borderRadius = '15px'
+
+/* helper function to loop through array and generate a book info card for each element */
+
+function generateLibrary () {
     for (let element of myLibrary) {
         let content = `<h2>${element.title}</h2>
         <p>by ${element.author}</p>
          <p>${element.pages} pages</p>
-         <p>Read? ${element.read}</p>
+         <p>Read: ${element.read}</p>
          `;
         bookCard.innerHTML = content;
         contentDiv.appendChild(bookCard.cloneNode(true));
-        
-
     }
 }
 
+function addBookToLibrary() {
+    while (contentDiv.firstChild) {
+        contentDiv.removeChild(contentDiv.lastChild)
+    } /* Remove old cards, so the entire array doesn't get repeated each time function is called */
+    let newBook = new book();
+    newBook.title = titleInput.value;
+    newBook.author = authorInput.value;
+    newBook.pages = pagesInput.value;
+    newBook.read = readInput.value;
+    myLibrary.push(newBook);
+    generateLibrary();
+}
 
+const submitButton = document.getElementById('submit');
 
-
-
-/* let bookButton = document.getElementById('newBookButton'); 
-
-bookButton.addEventListener('click', addBookToLibrary()); */
+submitButton.addEventListener('click', function(){
+    addBookToLibrary()
+})
